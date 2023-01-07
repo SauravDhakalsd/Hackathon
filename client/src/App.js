f15ffd709f78936c037827703d5dd0f00e5e5510
Saurav Dhakal <sauravdhakal.work@gmail.com>
2023-01-07 22:29:06 +0545

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,8 @@
 import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
 
 function App() {
   return <div></div>;
